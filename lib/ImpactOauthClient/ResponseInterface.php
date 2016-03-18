5007130bd78bb25b83d163b970edb4c4700b46f9
Jason Woys <jason@woys.org>
2016-03-17 17:24:33 -0700

    add interface file

---
+++ lib/ImpactOauthClient/ResponseInterface.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace ImpactOauthClient;
+
+/**
+ * Interface ResponseInterface
+ * @package ImpactOauthClient
+ */
+interface ResponseInterface
+{
+    /**
+     * @return mixed
+     */
+    public function getResponseCode();
+}
